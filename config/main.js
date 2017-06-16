cb34e88beeabdf5a78b790173e776786908fb914
stefanm1801 <stefanm1801@gmail.com>
2017-06-16 16:03:55 +0200

    Add files via upload

---
+++ config/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+    app: {
+        port: 8000,
+        credentials: {
+            username: 'edit',
+            password: 'edit2017'
+        }
+    },
+    api: {
+        credentials: {
+            username: 'Comtrade_mario',
+            password: 'KeepCalm123#'
+        },
+        baseUrl: '10.89.40.4',
+        port: 8081,
+        authAction: '/v1/authentication/user'
+    }
+};
